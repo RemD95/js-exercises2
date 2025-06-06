0c3d5765ba1e1fa2e26cbeef820cb5cb535526e4
Remus Daniel Blaj <remoblaj@gmail.com>
2025-06-06 17:31:44 +0200

    utilizzare il localStorage

--- index4.js
+++ index4.js
@@ -1,4 +1,5 @@
 /* gestire i cookies */
+
 function setCookie(name, value) {
     const d = new Date();
     d.setTime(d.getTime() + 48 * 60 * 60 * 1000);
@@ -9,8 +10,24 @@ setCookie("My-Data", "lorem");
 console.log("Cookies:", document.cookie);
 
 /*-----------------------------------------------------*/
-/*  */
+
+/* utilizzare il localStorage */
+
+const elemButton = document.getElementById("set-button")
+const elemCar = document.getElementById("Car")
+
+const carName = localStorage.getItem("my_dream_car") || "";
+elemCar.innerText = carName;
+
+elemButton.addEventListener("click", function () {
+    const carName = prompt("type here your dream car model:")
+    localStorage.setItem("my_dream_car", carName)
+    elemCar.innerText = carName
+})
+console.log(carName);
+
 /*-----------------------------------------------------*/
+
 /*  */
 /*-----------------------------------------------------*/
 /*  */
